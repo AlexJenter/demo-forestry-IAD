ddbea8da50516e95d0bf0317ca1d1cf9c7ff7c2c
Alex Jenter <jenteralex@gmail.com>
2020-06-26 02:07:05 +0200

    Header

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 const yaml = require("js-yaml");
+const CleanCSS = require("clean-css");
 
 module.exports = (config) => {
   config.addDataExtension("yaml", (contents) => yaml.safeLoad(contents));
+  config.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
 
   return {
     dir: {
       input: "src",
       output: "dist",
-      layouts: "_layouts",
     },
   };
 };
