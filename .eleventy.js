815c11d7e44860393c86d47861a63c72568f4510
Alex Jenter <jenteralex@gmail.com>
2020-06-26 16:53:30 +0200

    use token for header color

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,10 @@ const yaml = require("js-yaml");
 const CleanCSS = require("clean-css");
 
 module.exports = (config) => {
-  config.addDataExtension("yaml", (contents) => yaml.safeLoad(contents));
+  config.addDataExtension("yml", (contents) => yaml.safeLoad(contents));
+
   config.addFilter("cssmin", (code) => new CleanCSS({}).minify(code).styles);
+
   config.addCollection("pageSorted", function (collectionApi) {
     return collectionApi.getFilteredByTag("page").sort(function (a, b) {
       return a.data.sort_order - b.data.sort_order;
